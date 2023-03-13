6a0b9537a94ed040476d406849d9beaa91b6d133
aminmk99 <amirzakuchaki@gmail.com>
2023-03-13 18:36:01 +0330

    Created travel model

---
+++ lib/model/travel_model.dart
@@ -0,0 +1,23 @@
+class TravelModel {
+  String image;
+  String name;
+  String location;
+  String distance;
+  String temp;
+  String rating;
+  String description;
+  String price;
+
+  TravelModel(
+    {
+      required this.image,
+      required this.name,
+      required this.location,
+      required this.distance,
+      required this.temp,
+      required this.rating,
+      required this.description,
+      required this.price,
+    }
+  );
+}
